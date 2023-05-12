46d9ef008920b0b88059828591bd44f3ae70270b
Zuzu <7b4s8zl@code.edu.az>
2023-05-12 21:52:25 +0400

    Fiorello

---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+function myFunctionAll(){
+    var flowers = document.querySelectorAll(".item");
+    flowers.forEach(function(el) {
+      if (el.class == ".item") {
+        el.style.display = "none";
+      } else {
+        el.style.display = "block";
+      }
+    });
+  }
+function myFunction1() {
+    var flowers = document.querySelectorAll(".item");
+    flowers.forEach(function(el) {
+      if (el.id !== "cactus") {
+        el.style.display = "none";
+      } else {
+        el.style.display = "block";
+      }
+    });
+  }
+  
+  function myFunction2() {
+    var flowers = document.querySelectorAll(".item");
+    flowers.forEach(function(el) {
+      if (el.id !== "exotic") {
+        el.style.display = "none";
+      } else {
+        el.style.display = "block";
+      }
+    });
+  }
+
+  function myFunction3() {
+    var flowers = document.querySelectorAll(".item");
+    flowers.forEach(function(el) {
+      if (el.id !== "greens") {
+        el.style.display = "none";
+      } else {
+        el.style.display = "block";
+      }
+    });
+  }
+
+  function myFunction4() {
+    var flowers = document.querySelectorAll(".item");
+    flowers.forEach(function(el) {
+      if (el.id !== "popular") {
+        el.style.display = "none";
+      } else {
+        el.style.display = "block";
+      }
+    });
+  }
+
+  function myFunction5() {
+    var flowers = document.querySelectorAll(".item");
+    flowers.forEach(function(el) {
+      if (el.id !== "various") {
+        el.style.display = "none";
+      } else {
+        el.style.display = "block";
+      }
+    });
+  }
+
+  function myFunction6() {
+    var flowers = document.querySelectorAll(".item");
+    flowers.forEach(function(el) {
+      if (el.id !== "winter") {
+        el.style.display = "none";
+      } else {
+        el.style.display = "block";
+      }
+    });
+  }
+
+  
